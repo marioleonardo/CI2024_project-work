8917a83c61a2336d09c270bb09a7aa2123ec9003
Attrovio_Mario <44499977+marioleonardo@users.noreply.github.com>
2025-02-11 22:00:17 +0100

    modified for next exam better results

--- s329299.py
+++ s329299.py
@@ -1,26 +1,21 @@
-# Copyright © 2024 Giovanni Squillero <giovanni.squillero@polito.it>
-# https://github.com/squillero/computational-intelligence
-# Free under certain conditions — see the license for details.
+
 
 import numpy as np
-
-# All numpy's mathematical functions can be used in fornp.mulas
-# see: https://numpy.org/doc/stable/reference/routines.math.html
 
 
 def f0(x: np.ndarray) -> np.ndarray:
     return x[0] + np.sin(x[1]) / 5
 
 
-def f1(x: np.ndarray) -> np.ndarray: 
-    
+def f1(x: np.ndarray) -> np.ndarray:  
     return np.sin(x[0])
 
 def f2(x: np.ndarray) -> np.ndarray: 
-    return  1
+    return ((756207.936*x[2]+1218853.827*x[1]+1747375.474*np.arctan(0.4426*x[2]))*0.994*np.cos(0.502*x[0])+1247989.7133*np.sin(0.0269*x[1])+3000251.7633*np.arctan(1.089*x[0]))
 
-def f3(x: np.ndarray) -> np.ndarray: 
-    return np.add(np.subtract(np.multiply(-(x[1]), abs(np.multiply(x[1], abs(x[1])))), -(np.subtract(np.cos(np.multiply(x[0], x[0])), x[2]))), np.subtract(np.add(np.subtract(-(x[2]), -(np.add(np.subtract(np.multiply(-(x[2]), 0.931), -(max(np.multiply(x[0], x[0]), np.subtract(x[0], x[0])))), np.sqrt(np.subtract(x[0], x[0]))))), np.sqrt(max(0.0000000001,np.divide(np.tan(np.tan(-(-0.751))), -0.155)))), -(max(np.multiply(x[0], x[0]), np.subtract(x[0], x[0])))))
+
+def f3(x: np.ndarray) -> np.ndarray:
+    return np.round(((((x[2] * -3.5011236) - -4.2145568) + (-(x[1]) * np.square(x[1]))) - ((9.8732836e-07 - x[0]) * (x[0] + x[0]))) - 0.2143514, 7)
 
 def f4(x: np.ndarray) -> np.ndarray: 
 
@@ -44,17 +39,23 @@ def f4(x: np.ndarray) -> np.ndarray:
             return x - int(x)
         else:
             return np.array([fake_modf(xi) for xi in x])
+        
     return  np.add(np.add(np.cos(x[1]), np.add(np.add(0.8696509541, np.add(np.add(np.cos(np.negative(x[1])), np.cos(x[1])), np.add(np.cos(x[1]), np.add(np.tan(0.8696509541), np.cos(x[1]))))), np.add(np.cos(np.negative(x[1])), fake_modf(np.cos(np.negative(x[1])))))), fake_expa(np.tan(0.8696509541), np.tan(0.8696509541)))
 
 def f5(x: np.ndarray) -> np.ndarray: 
-    return np.multiply(np.multiply(np.subtract(np.add(x[0], x[1]), np.add(x[1], x[0])), np.multiply(np.divide(x[1], x[0]), np.subtract(0.384, 0.629))), np.subtract(-0.720, np.subtract(np.add(-0.502, x[1]), np.subtract(x[1], -0.558))))
+    return (np.cos(0.560802 * x[1]) * np.exp(1.27210 * x[0]) * np.arctan(0.3330 * x[0]) * (8.11230e-10 * 3.71410e-11 * 1.90540e-11)) / (np.exp(-0.98120 * x[1]) * (9.14440e-10 * 3.12660e-10 * 1.04880 * x[0]))
 
 def f6(x: np.ndarray) -> np.ndarray: 
     return np.subtract(x[1], np.multiply(np.subtract(x[0], x[1]), 0.695))
 
-def f7(x: np.ndarray) -> np.ndarray: 
-        
-    return  np.add(np.add(np.cos(x[1]), np.add(np.add(np.cos(x[1]), np.add(np.add(np.cos(x[1]), np.add(np.cos(x[1]), np.add(np.tan(0.990), np.add(np.cos(x[1]), np.cos(x[1]))))), np.cos(x[1]))), 0.939)), np.tan(0.683))
+def f7(x: np.ndarray) -> np.ndarray:
+    term1 = (x[1] * x[0]) * (1.298260 - np.sin((0.704873 - (x[1] * x[0])) * -0.125456))
+    term2 = ((0.941172 / np.power(0.919048, np.remainder(x[0], 0.179080))) *
+             np.power(np.power(1.007577, np.remainder(x[0], -0.545329)), ((x[1] - 0.545329) * x[0]))
+            ) - np.abs(np.arctan(np.arctan(x[0] - x[1])))
+    term3 = (1.774334 * (0.543759 + np.power(1.027270, np.remainder(-1.646242, x[1])))) + \
+            np.exp(np.power(0.970442, np.remainder(x[0], 0.965901)) + np.power(0.956161, (x[0] - x[1])))
+    return np.exp(np.square(0.194125 * (term1 + term2 * term3)))
 
 def f8(x: np.ndarray) -> np.ndarray: 
-    return np.multiply(max(max(np.multiply(np.multiply(x[5], np.abs(np.subtract(-0.37017066891978700482, x[5]))), np.negative(np.subtract(np.cos(np.sin(np.negative(x[5]))), x[5]))), np.abs(max(np.multiply(max(np.abs(x[4]), np.abs(-0.48552610290267739224)), np.negative(np.multiply(x[5], np.abs(x[5])))), 0.75612783915036763105))), np.abs(0.05160937174649449233)), np.subtract(np.divide(x[5], 0.05160937174649449233), np.subtract(np.add(x[5], x[0]), np.multiply(x[5], np.abs(np.negative(x[5]))))))
+    return 0.001*np.abs(x[2])+2933.8781*np.tan(0.28*x[5])-574.14*np.abs(x[4])+409.5379*np.cos(5.5364e-7*x[0])+406.5744*np.cos(0.084*x[1])-1848.7071*np.sin(0.641*x[5])-624.0142*np.cos(1.0164*x[4])+45.4951*x[3]
--- tree.py
+++ tree.py
@@ -49,7 +49,7 @@ class Tree:
                 sub_expression, sub_expression_val = self._generate_sub_expression(current_depth + 1)
                 sub_expression.tree_head = sub_expression_val
                 expression.branches.append(sub_expression)
-
+ 
                 assert sub_expression.tree_head is not None
             return expression, chosen_operator
 
@@ -76,54 +76,7 @@ class Tree:
 
         return target_node
 
-    def _sub_expression_mutation(self):
-        """Replace a random sub-expression with another randomly generated sub-expression."""
-
-        all_nodes = self._gather_all_nodes()
-        sub_expression, current_depth = random.choice(all_nodes)
-        branch_count = len(sub_expression.branches)
-
-        if branch_count > 1:
-            sub_expression.branches[random.randint(0, branch_count - 1)], _ = self._generate_sub_expression(
-                current_depth + 1)
-        elif branch_count == 1:
-            sub_expression.branches[0], _ = self._generate_sub_expression(current_depth + 1)
-
-    def _permutation_mutation(self):
-        """Shuffle the operands of a randomly selected sub-expression"""
-
-        all_nodes = self._gather_all_nodes()
-        target_node, _ = random.choice(all_nodes)
-        random.shuffle(target_node.branches)
-
-    def _subtree_replacement_mutation(self):
-        """Replace the entire expression with a randomly selected sub-expression."""
-        all_nodes = self._gather_all_nodes()
-        if len(all_nodes) > 1:
-            sub_expression, _ = random.choice(all_nodes[1:])
-            self.tree_head = sub_expression.tree_head
-            self.branches = sub_expression.branches
-
-        assert self._gather_all_nodes()
-
-    def _expansion_mutation(self):
-        """Expand a randomly chosen terminal node into an operator with operands."""
-
-        terminal_nodes = self._gather_all_terminal_nodes()
-        if terminal_nodes:
-            target_node = random.choice(terminal_nodes)
-            new_sub_expression, _ = self._generate_sub_expression(0)
-            target_node.tree_head = new_sub_expression.tree_head
-            target_node.branches = new_sub_expression.branches
-
-    def _collapsing_mutation(self):
-        """Collapse a randomly chosen sub-expression into a terminal node."""
-        all_nodes = self._gather_all_nodes()
-        if len(all_nodes) > 1:
-            sub_expression, _ = random.choice(all_nodes[1:])
-            terminal_val = random.choice(self.input_vars + self.constant_vals)
-            sub_expression.tree_head = terminal_val
-            sub_expression.branches = []
+    
 
     @staticmethod
     def _apply_mutation(expression: "Tree") -> "Tree":
@@ -148,57 +101,8 @@ class Tree:
 
         return recombined_expression
 
-    def render_expression(self):
-        """Render the expression tree using networkx in a tree-like structure."""
-        graph = nx.DiGraph()
-        node_labels = {}
-        node_counter = 0
+    
 
-        def add_graph_nodes(node, parent_node=None):
-            nonlocal node_counter
-            current_node_id = node_counter
-            node_counter += 1
-            if parent_node is not None:
-                graph.add_edge(parent_node, current_node_id)
-            node_labels[current_node_id] = str(node.tree_head.op_symbol) if node.branches else (
-                str(f"{node.tree_head:.3f}") if isinstance(node.tree_head, float) else str(node.tree_head))
-            for branch in node.branches:
-                add_graph_nodes(branch, current_node_id)
-
-        def calculate_tree_layout(graph: nx.DiGraph, root_node, horizontal_spacing=1., level_spacing=0.2,
-                                  vertical_position=0, horizontal_center=0.5):
-            """
-            If there is a cycle, then this will produce a hierarchy, but nodes will be repeated.
-                graph: the graph (must be a tree)
-                root_node: the root node of current branch
-                horizontal_spacing: horizontal space allocated for this branch - avoids overlap with other branches
-                level_spacing: gap between levels of hierarchy
-                vertical_position: vertical location of root
-                horizontal_center: horizontal location of root
-            """
-            node_positions = {root_node: (horizontal_center, vertical_position)}
-            neighbor_nodes = list(graph.neighbors(root_node))
-            if len(neighbor_nodes) != 0:
-                horizontal_delta = horizontal_spacing / len(neighbor_nodes)
-                next_horizontal = horizontal_center - horizontal_spacing / 2 - horizontal_delta / 2
-                for neighbor in neighbor_nodes:
-                    next_horizontal += horizontal_delta
-                    node_positions.update(calculate_tree_layout(graph, neighbor, horizontal_spacing=horizontal_delta,
-                                                               level_spacing=level_spacing,
-                                                               vertical_position=vertical_position - level_spacing,
-                                                               horizontal_center=next_horizontal))
-            return node_positions
-
-        plt.figure(figsize=(14, 8))
-
-        add_graph_nodes(self)
-
-        if not graph.nodes:
-            graph.add_node(0)
-
-        node_positions = calculate_tree_layout(graph, min(graph.nodes))
-        nx.draw(graph, node_positions, with_labels=True, labels=node_labels, node_size=400, node_color='lightblue',
-                font_size=10)
 
     def _gather_all_nodes(self):
         """Helper function to collect all nodes in the expression."""
@@ -263,103 +167,3 @@ class Tree:
 
         return traverse_tree(self)
 
-if __name__ == "__main__":
-
-    # Define some example operators and variables
-    common_operators = [
-        Operator('+', operator.add, 2),
-        Operator('-', operator.sub, 2),
-        Operator('*', operator.mul, 2),
-        Operator('/', operator.truediv, 2),
-        Operator('^', operator.pow, 2),
-        Operator('|.|', operator.abs, 1),
-        Operator('-', operator.neg, 1)
-    ]
-
-    extended_operators = [
-        Operator("|.|", np.abs, 1),
-        Operator("-", np.negative, 1),
-        Operator("+", np.add, 2),
-        Operator("-", np.subtract, 2),
-        Operator("*", np.multiply, 2),
-        Operator("/", np.divide, 2),
-        Operator("^", np.power, 2),
-        Operator("sign", np.sign, 1),
-        Operator("exp", np.exp, 1),
-        Operator("exp2", np.exp2, 1),
-        Operator("sqrt", np.sqrt, 1),
-        Operator("square", np.square, 1),
-        Operator("cbrt", np.cbrt, 1),
-        Operator("reciprocal", np.reciprocal, 1),
-        Operator("sin", np.sin, 1),
-        Operator("cos", np.cos, 1),
-        Operator("tan", np.tan, 1),
-        Operator("sinh", np.sinh, 1),
-        Operator("cosh", np.cosh, 1),
-        Operator("tanh", np.tanh, 1)
-    ]
-
-    special_operators = [
-        Operator("log", np.log, 1),
-        Operator("log2", np.log2, 1),
-        Operator("log10", np.log10, 1),
-        Operator("arcsin", np.arcsin, 1),
-        Operator("arccos", np.arccos, 1),
-        Operator("arctan", np.arctan, 1)
-    ]
-
-    full_operator_set = common_operators + extended_operators
-
-    input_variables = ['x0', 'x1']
-    constant_values = [10 * random.random() for _ in range(5)]
-    data = np.load("./data/problem_0.npz")
-
-    for i in range(100):
-        expression = Tree.construct_expression(depth_limit=5, operator_set=full_operator_set,
-                                                         input_vars=input_variables, constant_vals=constant_values)
-        print(f"Original Expression: {expression}")
-        expression.render_expression()
-
-        X = np.random.rand(2, 3)
-        y = np.random.randint(0, 10, 3)
-
-        print(f"Shapes: {X.shape}, {y.shape}")
-
-        print(Tree.calculate_mean_squared_error(expression, X, y))
-
-        expression = Tree._apply_mutation(expression)
-        print(f"After Mutation: {expression}")
-        expression.render_expression()
-
-        # expression._sub_expression_mutation()
-        # print("After Sub-expression Mutation:")
-        # expression.render_expression()
-
-        # expression._permutation_mutation()
-        # print("After Permutation Mutation:")
-        # expression.render_expression()
-
-        # expression._subtree_replacement_mutation()
-        # print("After Subtree Replacement Mutation:")
-        # expression.render_expression()
-
-        # expression._expansion_mutation()
-        # print("After Expansion Mutation:")
-        # expression.render_expression()
-
-        # expression._collapsing_mutation()
-        # print("After Collapsing Mutation:")
-        # expression.render_expression()
-
-        # other_expression = ExpressionTree.construct_expression(depth_limit=10, operator_set=full_operator_set,
-        #                                                      input_vars=input_variables,
-        #                                                      constant_vals=constant_values)
-        # print("Other Individual Expression:")
-        # other_expression.render_expression()
-
-        # ExpressionTree._recombine_expressions(expression, other_expression)
-        # print("After Sub-expression Exchange:")
-        # expression.render_expression()
-        # other_expression.render_expression()
-
-        # plt.show()
